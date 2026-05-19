20fd7560f1f28e95f886d85b2e0edda1eb40d023
Anh-pham-csc <st24456@ormiston.school.nz>
2026-05-20 09:55:06 +1200

    test

--- purebite.py
+++ purebite.py
@@ -1,2 +1,17 @@
 import tkinter as tk
-print("test")
+import customtkinter as ctk
+from tkinter import messagebox, ttk
+import sqlite3
+from flask import Flask, render_template
+import os
+from PIL import Image, ImageTk
+from datetime import datetime
+
+window = tk.Tk()
+window.geometry("730x690")
+window.title("OSC Dining 101")
+
+
+
+
+window.mainloop()
