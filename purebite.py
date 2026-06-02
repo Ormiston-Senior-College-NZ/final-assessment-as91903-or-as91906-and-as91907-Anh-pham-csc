46ddab4bc3dea56457fdd462a2620348ea7c8c86
Anh-pham-csc <st24456@ormiston.school.nz>
2026-06-03 09:10:38 +1200

    Update purebite.py

--- purebite.py
+++ purebite.py
@@ -1,17 +1,1 @@
-import tkinter as tk
-import customtkinter as ctk
-from tkinter import messagebox, ttk
-import sqlite3
-from flask import Flask, render_template
-import os
-from PIL import Image, ImageTk
-from datetime import datetime
 
-window = tk.Tk()
-window.geometry("730x690")
-window.title("OSC Dining 101")
-
-
-
-
-window.mainloop()
