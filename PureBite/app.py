caaa03b843745266b02181fc168eb2691d9830dd
Anh-pham-csc <st24456@ormiston.school.nz>
2026-06-08 14:29:57 +1200

    create + link a css file and code to collect + print the first food name input

--- PureBite/app.py
+++ PureBite/app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/check', methods=['GET', 'POST'])
+def check():
+    if request.method == 'POST':
+        name = request.form['stfoodname']
+        print(name)
+    return render_template('name.html')
+@app.route('/')
+def home():
+    return redirect(url_for('check'))
 
 if __name__ == '__main__':
     app.run(debug=True)
