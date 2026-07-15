213edb98f725571c693caf566fe829d32e82a53c
Anh-pham-csc <st24456@ormiston.school.nz>
2026-07-15 18:52:59 +1200

    add database (foods' names and chemical properties)

--- PureBite/app.py
+++ PureBite/app.py
@@ -36,22 +36,103 @@ def create_database():
             message TEXT NOT NULL)
     """)
 
-    foods = [
-        ("milk", "dairy"),
-        ("cheese", "dairy"),
-        ("yoghurt", "dairy"),
-        ("orange", "acidic"),
-        ("lemon", "acidic"),
-        ("lime", "acidic"),
-        ("banana", "fruit"),
-        ("apple", "fruit"),
-        ("fish", "protein"),
-        ("chicken", "protein"),
-        ("egg", "protein"),
-        ("energy drink", "caffeinated"),
-        ("alcohol", "alcohol"),
-    ]
- 
+    FOOD_GROUPS = {
+        "dairy": [
+            "milk", "skim milk", "whole milk",
+            "cheese", "mozzarella", "parmesan",
+            "yoghurt", "Greek yoghurt", "butter",
+            "cream", "sour cream", "ice cream",
+            "ghee", "cream cheese", "buttermilk",
+            "condensed milk", "evaporated milk",
+            "milk powder",
+        ], 
+
+        "acidic": [
+            "orange", "lemon", "lime", "grapefruit",
+            "apple", "passionfruit", "cranberry",
+        ],
+
+        "fruit": [
+            "banana", "apple", "pear", "mango",
+            "strawberry","coconut", "pineapple",
+            "kiwi", "blueberry", "raspberry", "blackberry",
+            "cherry", "watermelon", "papaya", "peach",
+            "plum", "grape", "pomegranate", "fig",
+            "date", "dragon fruit", "lychee", "guava",
+        ],
+
+        "protein": [
+            "beef", "chicken", "turkey", "pork", "lamb",
+            "venison", "duck", "salmon", "tuna", "sardine",
+            "mackerel", "cod", "shrimp", "crab", "lobster",
+            "mussels", "scallops", "clams", "oysters",
+            "cockles", "egg", "tofu", "tempeh", "lentils",
+            "chickpeas", "black beans", "soybeans", "peanuts",
+            "peanut butter", "almonds", "walnuts",
+            "pistachios", "cashwes", "pumpkin seeds",
+            "chia seeds", "flaxseed", "quinoa", "spirulina",
+        ], 
+
+        "starches": [
+            "white rice", "brown rice", "basmati rice",
+            "jasmine rice","wheat", "bread", "whole-grain bread",
+            "pasta", "noodles", "udon", "soba", "rice noodles",
+            "vermicelli", "corn", "cornmeal", "tortilla",
+            "oats", "barley", "rye", "buckwheat", "millet",
+            "sorghum", "potatoes", "sweet potatoes", "yams", 
+            "cassava", "tapioca", "cereal", "granola", 
+            "crackers", "pretzels", "bagels", "pancakes",
+            "waffles", "muffins", "donuts", "croissant",
+            "pizza crust",
+        ], 
+
+        "fats":[
+            "olive oil", "avocado", "coconut oil",
+            "canola oil", "peanut oil", "sesame oil",
+            "sunflower oil", "lard", "tallow",
+            "mayonnaise", "almond butter", "cashew butter",
+            "walnut oil", "flaxseed oil",
+            "pumpkin seed oil", "macadamia nuts",
+            "hazelnuts",
+
+        ],
+
+        "vegetables": [
+            "carrot", "broccoli", "cauliflower",
+            "spinach", "kale", "tomato", "cucumber",
+            "lettuce", "celery", "beetroot", "onion",
+            "garlic", "bell pepper","zucchini",
+            "eggplant", "pumpkin", "squash", "asparagus",
+            "green beans", "peas", "cabbage", "okra",
+            "radish", "turnip", "parsnip", 
+            "brussels sprouts", "artichoke", "leek",
+            "ginger", "turmetic"
+        ], 
+
+        "sugar": [
+            "honey", "maple syrup", "brown sugar",
+            "white sugar", "molasses", "jam",
+            "chocolate", "candy",
+        ], 
+
+        "fermented foods": [
+            "kimchi", "sauerkraut", "miso",
+            "kombucha", "kefir", "sourdough",
+            "pickles", "fish sauce",
+        ], 
+
+        "caffeinated": [
+            "energy drink",
+            "coffee",
+            "tea", 
+        ], 
+
+        "alcohol": [
+            "beer", 
+            "wine",
+            "alcohol,"
+        ]
+    }
     rules = [
         ("dairy", "acidic", "Caution", 
          "This combination can cause dairy to curdle. It is usually not harmful, but some people may find it uncomfortable."
@@ -59,7 +140,17 @@ def create_database():
 
         ("alcohol", "caffeinated", "Avoid",
          "Caffein can hide the effects of alcohol and make it easier to drink more intended."),
+
+        ("proteins", "proteins", "Avoid",
+         "Different proteins digest at different rates and need") 
     ]
+    
+    foods = []
+    
+    for food_group, food_names in FOOD_GROUPS.items():
+        for food_name in food_names:
+            foods.append((food_name, food_group))
+
 
     cursor.executemany(
         "INSERT OR IGNORE INTO foods (name, food_group) VALUES (?, ?)",
